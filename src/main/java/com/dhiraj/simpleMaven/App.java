0cfb4432858776de46225175cbf7ab178dd3f4d2
Patil1234 <you@example.com>
2020-05-23 19:34:16 +0530

    new code

---
+++ src/main/java/com/dhiraj/simpleMaven/App.java
@@ -0,0 +1,13 @@
+package com.dhiraj.simpleMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
